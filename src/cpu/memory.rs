eb38ffc21a6ed16af1dde45eb5d0bbb2d78a118e
Mark Goodwin <m0good04@gmail.com>
2023-10-09 16:03:42 -0400

    Added 16-bit value getter and setter for the Memory struct.

--- src/cpu/memory.rs
+++ src/cpu/memory.rs
@@ -11,18 +11,44 @@ impl Memory {
     }
   }
 
+  pub fn get_8_bit_value(&self, location: usize) -> u8 {
+    self.memory[location]
+  }
+
   pub fn set_8_bit_value(&mut self, location: usize, value: u8) {
     self.memory[location] = value;
   }
+  
+  pub fn get_16_bit_value(self, location: usize) -> u16 {
+    let lsb = self.memory[location];
+    let msb = self.memory[location + 1];
 
-  pub fn get_8_bit_value(&self, location: usize) -> u8 {
-    self.memory[location]
+    (msb as u16) << 8 | lsb as u16
+  }
+
+  pub fn set_16_bit_value(&mut self, location: usize, value: u16) {
+    let lsb = (value as u16) & 0x00ff;
+    let msb = (value as u16) >> 8;
+
+    self.memory[location] = lsb as u8;
+    self.memory[location + 1] = msb as u8;
   }
 }
 
 #[cfg(test)]
 mod tests {
   use super::*;
+
+  #[test]
+  fn test_get_8_bit_value() {
+    let mut memory = Memory::new();
+
+    memory.memory[0] = 0xff;
+
+    let value = memory.get_8_bit_value(0);
+
+    assert_eq!(value, 0xff);
+  }
 
   #[test]
   fn test_set_8_bit_value() {
@@ -36,15 +62,27 @@ mod tests {
   }
 
   #[test]
-  fn test_get_8_bit_value() {
+  fn test_get_16_bit_value() {
+    let mut memory = Memory::new();
+
+    memory.memory[0] = 0x2c;
+    memory.memory[1] = 0xfd;
+
+    let value = memory.get_16_bit_value(0);
+    
+    assert_eq!(value, 0xfd2c)
+  }
+
+  #[test]
+  fn test_set_16_bit_value() {
     let mut memory = Memory::new();
 
     assert_eq!(memory.memory[0], 0x00);
+    assert_eq!(memory.memory[1], 0x00);
 
-    memory.memory[0] = 0xff;
+    memory.set_16_bit_value(0, 0x2cfd);
 
-    let value = memory.get_8_bit_value(0);
-
-    assert_eq!(value, 0xff);
+    assert_eq!(memory.memory[0], 0xfd);
+    assert_eq!(memory.memory[1], 0x2c);
   }
  }

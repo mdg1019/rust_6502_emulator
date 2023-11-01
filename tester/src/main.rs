117f3ece9d93b675b22f2a72369fdbf84bacf432
Mark Goodwin <m0good04@gmail.com>
2023-11-01 15:59:06 -0400

    Passes the standalone decimal test too.

--- tester/src/main.rs
+++ tester/src/main.rs
@@ -8,9 +8,13 @@ fn main() {
 
     cpu.power_up();
 
+    // let _ = cpu
+    //     .memory
+    //     .read_raw_file_into_memory("/home/mark/6502TestFiles/6502_functional_test.bin", 0x0000);
+
     let _ = cpu
         .memory
-        .read_raw_file_into_memory("/home/mark/6502TestFiles/6502_functional_test.bin", 0x0000);
+        .read_raw_file_into_memory("/home/mark/6502TestFiles/6502_decimal_test.bin", 0x0000);
 
     cpu.run(Some(|s: &str| {
         println!("{}", s);
